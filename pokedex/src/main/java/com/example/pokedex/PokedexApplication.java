6cd1fd3f5e97415e3fe46fb3bb7956f94b51d683
YanYamim <yanyamim@gmail.com>
2025-02-21 15:20:01 -0300

    Botando essa pokedex como a versão Java

--- pokedex/src/main/java/com/example/pokedex/PokedexApplication.java
+++ pokedex/src/main/java/com/example/pokedex/PokedexApplication.java
@@ -3,6 +3,8 @@ package com.example.pokedex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Inicializar projeto
 @SpringBootApplication
 public class PokedexApplication {
 
