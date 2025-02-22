30885a5e0e6b55fa634c49ce15cd4d461dca937b
YanYamim <yanyamim@gmail.com>
2025-02-22 14:08:58 -0300

    Criando models da pokedex em python

--- pokedex/api/models.py
+++ pokedex/api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    id_pokemon = models.AutoField(primary_key=True)
+    nome_pokemon = models.CharField(max_length=20, default= '', null=False)
+    tipo_pokemon = models.CharField(max_length=8, default='')
+    tamanho = models.DecimalField(max_digits=5, decimal_places=2)
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
+    bioma = models.CharField(max_length=20, default='')
+
+    def __str__(self):
+        return f'Nome: {self.nome_pokemon} | Tipo: {self.tipo_pokemon}'
+    
+class Treinador(models.Model):
+    id_treinador = models.AutoField(primary_key=True)
+    nome_treinador = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'Nome: {self.nome_treinador} | Cidade: {self.cidade}'
+    
+class Ataque(models.Model):
+    id_ataque = models.AutoField(primary_key=True)
+    nome_ataque = models.CharField(max_length=50, null=False)
+    tipo_ataque = models.CharField(max_length=20)
+    dano = models.IntegerField(default=0)
+    precisao = models.IntegerField(default=0)
+    pp = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'Nome: {self.nome_ataque} | Cidade: {self.dano}'
+    
+class Regiao(models.Model):
+    id_regiao = models.AutoField(primary_key=True)
+    nome_regiao = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return f'Nome: {self.nome_regiao}'
--- pokedex/pokedex/settings.py
+++ pokedex/pokedex/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'pokedex.urls'
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'pokedex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
@@ -121,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_ORIGINS = [
+    'http://localhost:8080',
+]
