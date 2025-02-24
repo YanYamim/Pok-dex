84dae510ea47b83dc2b39c1cdf55bfbc3bc9074d
YanYamim <yanyamim@gmail.com>
2025-02-24 11:46:38 -0300

    Começando o método GET e serializer de Pokémons

--- pokedex/api/views.py
+++ pokedex/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Pokemon
+from .serializers import PokemonSerializer
+
+import json
+
+
+@api_view(['GET'])
+def listar_pokemons(request):
+    if request.method == 'GET':
+        pokemons = Pokemon.objects.all()
+
+        serializer = PokemonSerializer(pokemons, many=True)
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_404_NOT_FOUND)
